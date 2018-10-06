bf6be81fc56aa7591e273255a7a92296c6ef734f
HildaGD <hilda.guevara20@gmail.com>
2018-10-06 10:26:42 -0600

    eliminar

--- taller/src/pages/eliminar/eliminar.ts
+++ taller/src/pages/eliminar/eliminar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import {ConexionProvider}  from '../../providers/conexion/conexion';
 
 /**
  * Generated class for the EliminarPage page.
@@ -15,11 +16,64 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EliminarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  users:any[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+     public service:ConexionProvider, public alertCtrl: AlertController) {
+    this.readData();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EliminarPage');
+    
   }
+
+  readData(){
+    this.service.mostrarDatos().subscribe(
+      data=> this.users = data,
+      err => console.log(err)
+    );
+
+  }
+
+  eliminar(req){
+    console.log(req);
+    let confirm = this.alertCtrl.create({
+      title: 'Eliminar usuarrio?',
+      message: 'Desea eliminar el usuario selecionado?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: ()=> {
+            console.log('Agree clicked');
+            let params:any={
+              id_user:req.id_user
+            }
+            console.log(params);
+
+            this.service.eliminarUsuario(params)
+            .subscribe(
+            datos => {
+              //this.showAlert(datos.mensaje);
+              console.log(datos.mensaje)
+              //this.readData();
+              
+            },
+            err => console.log(err)
+            );
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  
 
 }
